90c0546e2c6e22374f6136cb4af79944df354e4d
A'sa Dickens <asadickens@gmail.com>
2024-04-28 11:07:04 -0400

    Updated Authentication Feature (#1)
    - added SignUp page
    - added Login Page
    - added AuthTextField widget
    - added AuthGradientButton widget

---
+++ flutter_mac/lib/features/auth/presentation/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mac/core/theme/app_pallete.dart';
+import 'package:flutter_mac/features/auth/presentation/pages/signup_page.dart';
+import 'package:flutter_mac/features/auth/presentation/widgets/auth_field.dart';
+import 'package:flutter_mac/features/auth/presentation/widgets/auth_gradient_button.dart';
+
+class LoginPage extends StatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const LoginPage(),
+      );
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailContorller = TextEditingController();
+  final passwordContorller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign In.",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              AuthField(
+                hintText: "Email",
+                controller: emailContorller,
+              ),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: "Password",
+                controller: passwordContorller,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 15),
+              const AuthGradientButton(text: "Sign In"),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    SignupPage.route(),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Don't have an Account? ",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: "Sign Up",
+                        style:
+                            Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  color: AppPallete.gradient2,
+                                ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
